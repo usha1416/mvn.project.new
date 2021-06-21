3cf600efbd7db2e3db507c03de15c439926089b3
usha <yusha1416@gmail.com>
2021-06-21 08:40:27 -0700

    deleted files

--- project2/src/main/java/test/com/SampleApp.java
+++
@@ -1,8 +0,0 @@
-package test.com;
-
-public class SampleApp {
-
-	public static void main(String[] args) {
-		System.out.println("Hello sample world!");
-	}
-}
--- web-apps/src/main/java/test/com/ServiceConfiguration.java
+++
@@ -1,6 +0,0 @@
-package test.com;
-
-import io.dropwizard.Configuration;
-
-public class ServiceConfiguration extends Configuration {
-}
